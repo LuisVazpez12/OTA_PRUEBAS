6839d38225df989c6a4a09f888154746bfd0f607
LuisVazpez12 <165457387+LuisVazpez12@users.noreply.github.com>
2024-08-23 19:00:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-from ota import OTAUpdater
-from WIFI import SSID, PASSWORD
 import machine,time
 from machine import Pin 
 
-firmware_url ="https://raw.githubusercontent.com/LuisVazpez12/OTA_PRUEBAS/main"
-
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
-ota_updater.download_and_install_update_if_available()
 led= Pin(2,Pin.OUT)
 led.on()
 time.sleep_ms(800)
