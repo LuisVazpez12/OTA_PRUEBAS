d8fcacdbf6d1c610e8da1e3f01bbc3be62aa598d
LuisVazpez12 <165457387+LuisVazpez12@users.noreply.github.com>
2024-08-26 17:57:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,15 @@ from WIFI_CONFIG import SSID, PASSWORD
 import machine,time
 from machine import Pin
 
-botUp = Pin(15, Pin.IN)
-led= Pin(2,Pin.OUT)
-led1= Pin(18,Pin.OUT)
-led2= Pin(19,Pin.OUT)
-led3= Pin(21,Pin.OUT)
-led4= Pin(22,Pin.OUT)
+botUp = Pin(7, Pin.IN)
+led= Pin(48,Pin.OUT)
 def upa():
     firmware_url = "https://github.com/LuisVazpez12/OTA_PRUEBAS/"
     ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
     ota_updater.download_and_install_update_if_available()
 def main():
-     led1.on()
-     led2.on()
-     led3.on()
-     led4.on()
+    coco=1
+
     
 while(1):
     if (botUp.value()==1):
